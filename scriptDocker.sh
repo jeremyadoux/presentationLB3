0a6388d4b2545bda336e9f3cfbecff17adb14f15
jeremyadoux <jeremy.adoux@gmail.com>
2017-10-18 16:52:09 +0200

    t
    t

--- scriptDocker.sh
+++ scriptDocker.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo curl -sSL https://get.docker.com | sudo sh
 
-curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.14.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 sudo mkdir /home/vagrant/mongodb
